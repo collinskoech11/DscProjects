0897c14d2e174feed841d24b3a263e6ac8cb4e84
Collins Koech <collinskoechck34@gmail.com>
2020-10-16 11:11:06 +0300

    app update

--- GikombaOnline/gikomba-store/src/App.js
+++ GikombaOnline/gikomba-store/src/App.js
@@ -4,13 +4,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Button/>
-=======
-  
-   <Home/>
-
->>>>>>> 0c8444b9313236ec78421216660c5ef4483fd67f
+    <Home/>
   );
 }
 
