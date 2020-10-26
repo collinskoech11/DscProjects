75b046870c047449e364655410cfb10a5981abb6
Diana-nyamai <dnyamai.dn@gmail.com>
2020-10-26 21:21:38 +0300

    update

--- GikombaOnline/gikomba-store/src/App.js
+++ GikombaOnline/gikomba-store/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Navigators/Navbar';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './pages/Home';
+import CustomFooter from './Navigators/CustomFooter';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
      <Navbar/>
      <Route exact path="/" component={Home}/>
     </div>
+    <CustomFooter/>
     </Router>
+    
   );
 }
 
